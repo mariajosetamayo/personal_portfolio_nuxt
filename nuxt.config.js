dff4b1ed3216f5e67e391ddb2f729d2a9284c15c
Maria Jose Tamayo <mjtamayo@Marias-MacBook-Pro.local>
2025-05-06 22:05:17 -0600

    added ssr

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,6 +27,8 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['@/assets/main.scss'],
 
+  ssr: true,
+
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
